802ebc365ae2f272cf0957575d219ceed77554ec
elena <creative.apme@gmail.com>
2023-12-03 22:40:15 +0900

    Initial commit

---
+++ src/main/kotlin/com/acceleratedev/AccelerateDevApplication.kt
@@ -0,0 +1,11 @@
+package com.acceleratedev
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccelerateDevApplication
+
+fun main(args: Array<String>) {
+    runApplication<AccelerateDevApplication>(*args)
+}
---
+++ src/test/kotlin/com/acceleratedev/AccelerateDevApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.acceleratedev
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccelerateDevApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
