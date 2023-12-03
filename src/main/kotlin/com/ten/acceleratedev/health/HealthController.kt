ccdb1978af1583c9cae8f3ecabdcab37d8040aba
elena <creative.apme@gmail.com>
2023-12-04 01:29:43 +0900

    fix: dir, yml files

--- src/main/kotlin/com/ten/acceleratedev/health/HealthController.kt
+++ src/main/kotlin/com/ten/acceleratedev/health/HealthController.kt
@@ -1,7 +1,7 @@
-package com.acceleratedev.health
+package com.ten.acceleratedev.health
 
-import com.acceleratedev.common.ApiResponse
-import com.acceleratedev.common.createApiResponse
+import com.ten.acceleratedev.common.ApiResponse
+import com.ten.acceleratedev.common.createApiResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
