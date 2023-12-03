09c181e162dc27c176c56529741f1d7e0ca05f75
elena <creative.apme@gmail.com>
2023-12-03 22:54:14 +0900

    feat: add HealthController

---
+++ src/main/kotlin/com/acceleratedev/common/ApiResponse.kt
@@ -0,0 +1,11 @@
+package com.acceleratedev.common
+
+data class ApiResponse(
+  val status: Int,
+  val message: String,
+  val data: Any?
+)
+
+fun <T> createApiResponse(status: Int = 200, message: String = "", data: T? = null): ApiResponse {
+  return ApiResponse(status, message, data)
+}
